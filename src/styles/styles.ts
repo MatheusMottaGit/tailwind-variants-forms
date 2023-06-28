ec013b8ef231f5bf0990ff952f1b8667a1f64abf
Matheus Motta <matheusdomingues423@gmail.com>
2023-06-28 16:00:15 -0300

    styles-taikwind-variants

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,20 +1,19 @@
 import { tv, VariantProps } from "tailwind-variants";
 
-export interface InputProps extends VariantProps<typeof input>{}
+export interface InputProps extends VariantProps<typeof form>{}
 
-export const input = tv({
+export const form = tv({
   slots: {
-    border: "none"
+    base: "flex w-80 flex-col items-center justify-center gap-4 text-white/40",
+    input: "w-full p-2 bg-zinc-800 outline-none",
+    button: "w-full p-2 bg-green-800 text-white cursor-pointer hover:bg-green-900"
   },
 
   variants: {
     state: {
       error: {
-        border: "border-2 border-red-800"
-      },
-
-      success: {
-        border: "border-2 border-green-700"
+        input: "border-2 border-red-800",
+        button: "bg-green-900 cursor-not-allowed text-white/50"
       }
     }
   }
