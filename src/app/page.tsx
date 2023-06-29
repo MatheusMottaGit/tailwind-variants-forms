8103049dd1adee1af25e2764a2b20fce8ddc7a0b
Matheus Motta <matheusdomingues423@gmail.com>
2023-06-29 11:12:30 -0300

    app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,70 +1,38 @@
 'use client'
-import { z } from "zod"
 import { useForm } from "react-hook-form"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { zodResolver } from '@hookform/resolvers/zod'
-import { VariantProps, tv } from "tailwind-variants"
+import { FormProps, form } from "@/styles/styles"
+import { UserData, createUserFormSchema } from "@/schema/formSchema"
 
 import ErrorMessage from "@/components/ErrorMessage"
 
-type UserData = z.infer<typeof createUserFormSchema>
+export default function Home({  }: FormProps) {
 
-const createUserFormSchema = z.object({
-
-  email: z.string()
-    .nonempty('Campo obrigatório...')
-    .email('Formato inválido...'),
-
-  password: z.string()
-    .min(8, 'Sua senha deve ter no mínimo 8 caracteres...')
-})
-
-interface FormProps extends VariantProps<typeof form>{}
-
-const form = tv({
-  slots: {
-    base: "flex w-80 flex-col items-center justify-center gap-4 text-white/40",
-    input: "w-full p-2 bg-zinc-800 outline-none",
-    button: "w-full p-2 bg-green-800 text-white cursor-pointer hover:bg-green-900"
-  },
-
-  variants: {
-    state: {
-      error: {
-        input: "border-2 border-red-800",
-        button: "bg-green-900 text-white/50 cursor-not-allowed"
-      },
-
-      success: {
-        input: "border-2 border-green-800"
-      }
-    }
-  }
-})
-
-export default function Home({ state }: FormProps) {
+  console.log('render')
 
   const [user, setUser] = useState('')
-
-  const { 
-    base, 
-    input, 
-    button 
-  } = form({ state })
-
+  const [state, setState] = useState<FormProps["state"]>()
+  
   const { 
     register, 
     handleSubmit, 
     formState: { errors },
     reset
   } = useForm<UserData>({ resolver: zodResolver(createUserFormSchema) })
+  
+  const { base, input, button } = form({ state })
+
+  useEffect(() =>{
+    if(errors.email && errors.password){
+      setState("error")
+    }
+  }, [errors])
 
   function createUser(data: UserData){
 
-    console.log(data)
-
     setUser(JSON.stringify(data, null, 2))
-
+    
     reset({
       email: "",
       password: ""
@@ -90,7 +58,7 @@ export default function Home({ state }: FormProps) {
           placeholder="Senha..."
           {...register('password')} 
         />
-        
+        {errors.password && <ErrorMessage>{errors.password.message}</ErrorMessage>}
       </div>
 
       <button className={button()} onClick={handleSubmit(createUser)}>
